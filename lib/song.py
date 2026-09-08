f251d39268b57c621076343788252b3a778e3d55
jeff <jefferykibet@gmail.com>
2026-09-08 09:36:28 +0300

    Implement Song class with attributes and initializer

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,11 @@
 class Song:
-    pass
+    count = 0
+    genre = []
+    artists = []
+    genre_count = []
+    artists_count = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
