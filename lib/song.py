6fb5eb6048a9c6a93ef0f64e27f9ab9064b634e4
jeff <jefferykibet@gmail.com>
2026-09-08 11:42:52 +0300

    Refactor Song class methods for genre and artist management

--- lib/song.py
+++ lib/song.py
@@ -28,3 +28,27 @@ class Song:
         """Add a genre to the genres list, keeping entries unique."""
         if genre not in cls.genres:
             cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Add an artist to the artists list, keeping entries unique."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Increment the count for this genre, creating the key
+        with a starting value of 1 if it doesn't exist yet."""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        """Increment the count for this artist, creating the key
+        with a starting value of 1 if it doesn't exist yet."""
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
