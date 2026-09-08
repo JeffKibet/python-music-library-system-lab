e5ee10db5f3fad6a38f4d3c624324885283e935d
jeff <jefferykibet@gmail.com>
2026-09-08 12:05:56 +0300

    Refactor Song class to use dictionaries for genre and artist counts, and update related methods for consistency

--- lib/song.py
+++ lib/song.py
@@ -1,9 +1,9 @@
 class Song:
     count = 0
-    genre = []
+    genres = []
     artists = []
-    genre_count = []
-    artists_count = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -14,44 +14,43 @@ class Song:
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
         Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
+        Song.add_to_artist_count(artist)
 
     def __repr__(self):
         return f"<Song: '{self.name}' by {self.artist} ({self.genre})>"
 
+    @classmethod
     def add_song_to_count(cls):
-        """Increment the total song count by one."""
+        
         cls.count += 1
 
     @classmethod
     def add_to_genres(cls, genre):
-        """Add a genre to the genres list, keeping entries unique."""
+        
         if genre not in cls.genres:
             cls.genres.append(genre)
 
     @classmethod
     def add_to_artists(cls, artist):
-        """Add an artist to the artists list, keeping entries unique."""
+        
         if artist not in cls.artists:
             cls.artists.append(artist)
 
     @classmethod
     def add_to_genre_count(cls, genre):
-        """Increment the count for this genre, creating the key
-        with a starting value of 1 if it doesn't exist yet."""
+        
         if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
             cls.genre_count[genre] = 1
 
     @classmethod
-    def add_to_artists_count(cls, artist):
-        """Increment the count for this artist, creating the key
-        with a starting value of 1 if it doesn't exist yet."""
-        if artist in cls.artists_count:
-            cls.artists_count[artist] += 1
+    def add_to_artist_count(cls, artist):
+        
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            cls.artists_count[artist] = 1
+            cls.artist_count[artist] = 1
 
 if __name__ == "__main__":
     song1 = Song("Alright", "Kendrick Lamar", "Rap")
