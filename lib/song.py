ca04ed36f197ec739ab3b3a466836979e88e8767
jeff <jefferykibet@gmail.com>
2026-09-08 09:38:07 +0300

    Refactor Song class initializer to include genre and artist tracking

--- lib/song.py
+++ lib/song.py
@@ -9,3 +9,9 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
